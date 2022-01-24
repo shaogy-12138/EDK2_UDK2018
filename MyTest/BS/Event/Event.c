b5b41bf462f7c80e1f7b742d881e9c7166727dd9
shaogy-12138 <shaogy12138@163.com>
2022-01-24 10:16:37 +0800

    mouse event

--- MyTest/BS/Event/Event.c
+++ MyTest/BS/Event/Event.c
@@ -9,6 +9,7 @@
 #include <Uefi.h>
 #include <Library/UefiBootServicesTableLib.h>
 #include <Library/UefiLib.h>
+#include <Protocol/SimplePointer.h>
 
 
 
@@ -71,9 +72,60 @@ EFI_STATUS TestNotify()
                              (VOID *)NULL,
                              &myEvent);
 
+    if(Status){  }
     Status = gBS->WaitForEvent(1, &myEvent, &index);
     return EFI_SUCCESS;
 }
+
+EFI_STATUS TestMouseSimple(){
+    EFI_STATUS          Status;
+    EFI_SIMPLE_POINTER_PROTOCOL* mouse = 0;
+    EFI_SIMPLE_POINTER_STATE     State;
+    EFI_EVENT           events[2]; //{0, gBS->ConIn->WaitForKey}
+
+    Print(L">>Debug2: start func\n");
+    // show cursor
+    gST->ConOut->EnableCursor(gST->ConOut, TRUE);
+    
+    Print(L">>Debug3: find mouse device\n");
+    // find mouse device
+    Status = gBS->LocateProtocol(&gEfiSimplePointerProtocolGuid, NULL, (VOID **)&mouse);
+
+    Print(L">>Debug4: reset mouse device\n");
+    //reset mouse device
+    Status = mouse->Reset(mouse, TRUE);
+    
+    events[0] = mouse->WaitForInput;
+    // input keyEvent to events[1]
+    events[1] = gST->ConIn->WaitForKey;
+
+    while(1){
+        EFI_INPUT_KEY           Key;
+        UINTN                   index;
+
+        // wait events occur
+        Status = gBS->WaitForEvent(2, events, &index);
+        if(Status){  }
+        
+        if(index == 0){
+            // get and output mouse state
+            Status = mouse->GetState(mouse, &State);
+            Print(L"X:%d Y:%d Z:%d L:%d R:%d\n",State.RelativeMovementX,State.RelativeMovementY,State.RelativeMovementZ,State.LeftButton, State.RightButton);
+        }else{
+            Status = gST->ConIn->ReadKeyStroke(gST->ConIn, &Key);
+            // press 'q', exit
+            if(Key.UnicodeChar == 'q')
+                break;
+            Print(L"%c", Key.UnicodeChar);
+        }
+    }
+
+    return EFI_SUCCESS;
+
+}
+
+
+
 
 EFI_STATUS  
 EFIAPI
@@ -83,7 +135,9 @@ UefiMain(
     )
 {
     EFI_STATUS Status;
+    Print(L">>Debug1: input func\n");
+    Status = TestMouseSimple();
     //Status = TestNotify();
-    Status = TestTimer();
+   // Status = TestTimer();
     return Status;
 }
